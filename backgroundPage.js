6f38ea234c14f4e998815bc5a2c7d8630e48e256
Brendan Abolivier <contact@brendanabolivier.com>
2016-09-16 22:10:30 +0200

    Switched event page to background page

--- backgroundPage.js
+++ backgroundPage.js
@@ -1,5 +1,6 @@
 sites_url = "https://raw.githubusercontent.com/RSF-RWB/collateralfreedom/master/sites.json"
 sites = {}
+domain_regexp = /:\/\/(www\.)?([^\/]+)\//
 
 function getSitesAndMirrors() {
     $.getJSON(sites_url).done(function(data) { chrome.storage.local.set({"sites": data}) })
@@ -26,18 +27,20 @@ function updateTab() {
     getCurrentTabUrl(function(url) {
         chrome.storage.local.get("sites", function(sites){
             sites = sites.sites
-            domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
-            if(domain in sites) {
-                chrome.browserAction.setIcon({path: 'rsc/icon-red.png'})
-            }
-            else {
-                chrome.browserAction.setIcon({path: 'rsc/icon.png'})
+            if(url.match(domain_regexp)) {
+                // Skipping detection for about:* pages
+                domain = url.match(domain_regexp).slice(-1)[0]
+                if(domain in sites) {
+                    chrome.browserAction.setIcon({path: 'rsc/icon-red.png'})
+                }
+                else {
+                    chrome.browserAction.setIcon({path: 'rsc/icon.png'})
+                }
             }
         })
     })
 }
 
-chrome.runtime.onStartup.addListener(getSitesAndMirrors)
-chrome.runtime.onInstalled.addListener(getSitesAndMirrors)
 chrome.tabs.onActivated.addListener(updateTab)
 chrome.tabs.onUpdated.addListener(updateTab)
+getSitesAndMirrors()
