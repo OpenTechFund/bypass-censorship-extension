5eba7d75ea65744ff6c94f58b75e992aa7a63d5a
Brendan Abolivier <contact@brendanabolivier.com>
2017-11-17 23:18:09 +0100

    Wait for document to be ready to be manipulated before manipulating it

--- index.js
+++ index.js
@@ -28,21 +28,30 @@ function takeMeTo(url) {
     chrome.tabs.update(updateProperties=updateProperties)
 }
 
-getCurrentTabUrl(function(url) {
-    chrome.storage.local.get("sites", function(sites){
-        sites = sites.sites
-        domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
-        if(domain in sites) {
-            proxies = sites[domain]
-            $("#mirror").css("display", "block")
-            $("#nomirror").css("display", "none")
-            $("#mirror button").on("click", function() {
-                takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
-            })
-        }
-        else {
-            $("#mirror").css("display", "none")
-            $("#nomirror").css("display", "block")
-        }
-    })
+// Wait for the DOM to load before doing anything
+jQuery(document).ready(function() {
+	// Check if there's a mirror for the current URL. If so, display the message
+	// about it and the button to redirect to a random mirror (if there's more than
+	// one).
+	getCurrentTabUrl(function(url) {
+		chrome.storage.local.get("sites", function(sites){
+			sites = sites.sites
+			// Grab the domain part of the URL
+			domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
+			if(domain in sites) {
+				proxies = sites[domain]
+				// Offer the user to redirect them to a mirror
+				$("#mirror").css("display", "block")
+				$("#nomirror").css("display", "none")
+				$("#mirror button").on("click", function() {
+					takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
+				})
+			}
+			else {
+				// Tell the user there's no mirror available
+				$("#mirror").css("display", "none")
+				$("#nomirror").css("display", "block")
+			}
+		})
+	})
 })
