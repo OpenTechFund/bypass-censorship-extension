6c8ae15f806fd602327bb804c6754c4bf74664f4
Brendan Abolivier <contact@brendanabolivier.com>
2017-11-17 21:29:58 +0100

    Reindent + doc

--- backgroundPage.js
+++ backgroundPage.js
@@ -2,45 +2,50 @@ sites_url = "https://raw.githubusercontent.com/RSF-RWB/collateralfreedom/master/
 sites = {}
 domain_regexp = /:\/\/(www\.)?([^\/]+)\//
 
+// Retrieve the list of mirrors
 function getSitesAndMirrors() {
-    $.getJSON(sites_url).done(function(data) { chrome.storage.local.set({"sites": data}) })
+	$.getJSON(sites_url).done(function(data) { chrome.storage.local.set({"sites": data}) })
 }
 
+// Get the URL on the current tab
 function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-
-    var url = tab.url;
-
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
+	chrome.tabs.query(queryInfo, function(tabs) {
+		var tab = tabs[0];
+		var url = tab.url;
+		console.assert(typeof url == 'string', 'tab.url should be a string');
+		callback(url);
+	});
 }
 
+// Check if the URL on the current tab is bound to a list of mirrors. If so,
+// indicate it to the user by turning the icon red.
 function updateTab() {
-    getCurrentTabUrl(function(url) {
-        chrome.storage.local.get("sites", function(sites){
-            sites = sites.sites
-            if(url.match(domain_regexp)) {
-                // Skipping detection for about:* pages
-                domain = url.match(domain_regexp).slice(-1)[0]
-                if(domain in sites) {
-                    chrome.browserAction.setIcon({path: 'rsc/icon-red.png'})
-                }
-                else {
-                    chrome.browserAction.setIcon({path: 'rsc/icon.png'})
-                }
-            }
-        })
-    })
+	getCurrentTabUrl(function(url) {
+		chrome.storage.local.get("sites", function(sites){
+			sites = sites.sites
+			if(url.match(domain_regexp)) {
+				// Skipping detection for about:* pages
+				domain = url.match(domain_regexp).slice(-1)[0]
+				if(domain in sites) {
+					chrome.browserAction.setIcon({path: 'rsc/icon-red.png'})
+				}
+				else {
+					chrome.browserAction.setIcon({path: 'rsc/icon.png'})
+				}
+			}
+		})
+	})
 }
 
+// Run the updateTab() function on tab switch (Ctrl+Tab), creation or target
+// update
 chrome.tabs.onActivated.addListener(updateTab)
 chrome.tabs.onUpdated.addListener(updateTab)
+
+// Fetch the list of mirrors on startup
 getSitesAndMirrors()
--- index.js
+++ index.js
@@ -2,47 +2,52 @@
  * Get the current URL.
  *
  * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
+ * is found.
  */
 function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
+	chrome.tabs.query(queryInfo, function(tabs) {
+		var tab = tabs[0];
+		var url = tab.url;
+		console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    var url = tab.url;
-
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
+		callback(url);
+	});
 }
 
+// Redirect to mirror url
 function takeMeTo(url) {
-    updateProperties = {
-        url: "https://" + url
-    }
-    chrome.tabs.update(updateProperties=updateProperties)
+	updateProperties = {
+			url: "https://" + url
+	}
+	chrome.tabs.update(updateProperties=updateProperties)
 }
 
+// Check if there's a mirror for the current URL. If so, display the message
+// about it and the button to redirect to a random mirror (if there's more than
+// one).
 getCurrentTabUrl(function(url) {
-    chrome.storage.local.get("sites", function(sites){
-        sites = sites.sites
-        domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
-        if(domain in sites) {
-            proxies = sites[domain]
-            $("#mirror").css("display", "block")
-            $("#nomirror").css("display", "none")
-            $("#mirror button").on("click", function() {
-                takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
-            })
-        }
-        else {
-            $("#mirror").css("display", "none")
-            $("#nomirror").css("display", "block")
-        }
-    })
+	chrome.storage.local.get("sites", function(sites){
+			sites = sites.sites
+			// Grab the domain part of the URL
+			domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
+			if(domain in sites) {
+					proxies = sites[domain]
+					// Offer the user to redirect them to a mirror
+					$("#mirror").css("display", "block")
+					$("#nomirror").css("display", "none")
+					$("#mirror button").on("click", function() {
+							takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
+					})
+			}
+			else {
+					// Tell the user there's no mirror available
+					$("#mirror").css("display", "none")
+					$("#nomirror").css("display", "block")
+			}
+	})
 })
