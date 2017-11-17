1eb01c88ed1b484b1fe0d38adc6baa75f6b76a0b
Brendan Abolivier <contact@brendanabolivier.com>
2017-11-17 23:18:09 +0100

    Wait for document to be ready to be manipulated before manipulating it

--- index.js
+++ index.js
@@ -27,27 +27,30 @@ function takeMeTo(url) {
 	chrome.tabs.update(updateProperties=updateProperties)
 }
 
-// Check if there's a mirror for the current URL. If so, display the message
-// about it and the button to redirect to a random mirror (if there's more than
-// one).
-getCurrentTabUrl(function(url) {
-	chrome.storage.local.get("sites", function(sites){
+// Wait for the DOM to load before doing anything
+jQuery(document).ready(function() {
+	// Check if there's a mirror for the current URL. If so, display the message
+	// about it and the button to redirect to a random mirror (if there's more than
+	// one).
+	getCurrentTabUrl(function(url) {
+		chrome.storage.local.get("sites", function(sites){
 			sites = sites.sites
 			// Grab the domain part of the URL
 			domain = url.match(/:\/\/(www\.)?([^\/]+)\//).slice(-1)[0]
 			if(domain in sites) {
-					proxies = sites[domain]
-					// Offer the user to redirect them to a mirror
-					$("#mirror").css("display", "block")
-					$("#nomirror").css("display", "none")
-					$("#mirror button").on("click", function() {
-							takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
-					})
+				proxies = sites[domain]
+				// Offer the user to redirect them to a mirror
+				$("#mirror").css("display", "block")
+				$("#nomirror").css("display", "none")
+				$("#mirror button").on("click", function() {
+					takeMeTo(proxies[Math.floor(Math.random()*proxies.length)])
+				})
 			}
 			else {
-					// Tell the user there's no mirror available
-					$("#mirror").css("display", "none")
-					$("#nomirror").css("display", "block")
+				// Tell the user there's no mirror available
+				$("#mirror").css("display", "none")
+				$("#nomirror").css("display", "block")
 			}
+		})
 	})
 })
