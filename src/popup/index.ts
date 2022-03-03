b8ec4d5c51e9633bf9469408297ba4da8ca38358
rudietuesdays <rae@throneless.tech>
2022-03-03 12:20:29 -0500

    add button to popup to copy tor links

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -14,21 +14,38 @@ async function popup() {
 	const message = document.createElement('div');
 	const hasMirror = domain && mirrors ? mirrors.has(domain) : false;
 	let proxies = [];
+	let bridges = [];
 	if (hasMirror) {
 		const alts = mirrors.get(domain);
 		proxies = [...alts].filter(item => item.proto === 'https');
+		bridges = [...alts].filter(item => item.type === 'tor' || item.type === 'eotk');
 	}
-	if (proxies.length > 0) {
-		message.innerText = browser.i18n.getMessage('mirror');
-		const button = document.createElement('button');
-		button.innerText = browser.i18n.getMessage('mirror_button');
-		button.addEventListener("click", async () => {
-			const proxy = proxies[Math.floor(Math.random() * proxies.length)];
-			await browser.tabs.update({ url: `${proxy.url}` });
-			window.close(); // TODO: won't work in Firefox Android
-		})
-		popup.appendChild(message);
-		popup.appendChild(button);
+	if (proxies.length || bridges.length) {
+		if (proxies.length) {
+			message.innerText = browser.i18n.getMessage('mirror');
+			const button = document.createElement('button');
+			button.innerText = browser.i18n.getMessage('mirror_button');
+			button.addEventListener("click", async () => {
+				const proxy = proxies[Math.floor(Math.random() * proxies.length)];
+				await browser.tabs.update({ url: `${proxy.url}` });
+				window.close(); // TODO: won't work in Firefox Android
+			})
+			popup.appendChild(message);
+			popup.appendChild(button);
+		} 
+		if (bridges.length) {
+			const button = document.createElement('button');
+			button.innerText = browser.i18n.getMessage('tor_button');
+			button.addEventListener("click", async () => {
+				const bridge = bridges[Math.floor(Math.random() * bridges.length)];
+				navigator.clipboard.writeText(bridge.url).then(function() {
+					popup.innerHTML += "Copied!";
+				}, function(err) {
+					popup.innerHTML += `Could not copy text: ${err}`;
+				});
+			})
+			popup.appendChild(button);
+		}
 	} else {
 		message.innerText = browser.i18n.getMessage('nomirror');
 		popup.appendChild(message);
